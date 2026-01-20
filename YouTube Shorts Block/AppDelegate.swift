ccf764a440da011b2a7e7b202d5aa947df034267
TopScrech <sergei_saliukov@icloud.com>
2026-01-20 01:37:02 +0100

    setup

--- YouTube Shorts Block/AppDelegate.swift
+++ YouTube Shorts Block/AppDelegate.swift
@@ -1,24 +1,15 @@
-//
-//  AppDelegate.swift
-//  YouTube Shorts Block
-//
-//  Created by Sergei Saliukov on 20.01.2026.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        // Override point for customization after application launch
+        true
     }
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
 }
--- YouTube Shorts Block/SceneDelegate.swift
+++ YouTube Shorts Block/SceneDelegate.swift
@@ -1,18 +1,9 @@
-//
-//  SceneDelegate.swift
-//  YouTube Shorts Block
-//
-//  Created by Sergei Saliukov on 20.01.2026.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
 }
